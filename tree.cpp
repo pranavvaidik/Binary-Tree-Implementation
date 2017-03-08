73be69567012c821f793b69fa7285a47262f17ff
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2017-03-08 03:24:21 -0600

    fixed bugs for insert_node and pre-order traversal

--- tree.cpp
+++ tree.cpp
@@ -5,22 +5,28 @@ struct node{
 	int value;
 	struct node *leftChild;
 	struct node *rightChild;
-}
+};
 
 
 class tree
 {
 public:
+	tree();
+	~tree();	
+	
 	void insert_node(int node_key);
 	node *search(int node_key);
-	void preOrder(node *n);
-	int height(node *n);
+	void preOrder();
+	int height();
 
 private:
-	void *search(int node_key, node *leaf);
-	void *node_key(int node_key, node *leaf);
+	node *search(int node_key, node *leaf);
+	void insert_node(int node_key, node *leaf);
+	int height(node *n);
+	void preOrder(node *n);	
 	node *root;
-}
+
+};
 
 tree::tree()
 {
@@ -37,26 +43,28 @@ void tree::insert_node(int node_key, node *leaf)
 	if(node_key < leaf->value)
 	{
 		if(leaf->leftChild != NULL) 
-			insert(node_key, leaf->leftChild);
+			insert_node(node_key, leaf->leftChild);
 		else
 		{
 			leaf->leftChild = new node;
 			leaf->leftChild->value = node_key;
-			leftChild->leftChild->leftChild = NULL;
-			leftChild->leftChild->rightChild = NULL;
+			leaf->leftChild->leftChild = NULL;
+			leaf->leftChild->rightChild = NULL;
+			//cout << "the left node value of "<< node_key << " was inserted successfully" << endl;
 		}
 	}
 
-	else if(node_key < leaf->value)
+	else if(node_key > leaf->value)
 	{
 		if(leaf->rightChild != NULL) 
-			insert(node_key, leaf->rightChild);
+			insert_node(node_key, leaf->rightChild);
 		else
 		{
 			leaf->rightChild = new node;
 			leaf->rightChild->value = node_key;
-			leftChild->rightChild->leftChild = NULL;
-			leftChild->rightChild->rightChild = NULL;
+			leaf->rightChild->leftChild = NULL;
+			leaf->rightChild->rightChild = NULL;
+			//cout << "the right node value of "<< node_key << " was inserted successfully" << endl;
 		}
 	}
 }
@@ -64,13 +72,14 @@ void tree::insert_node(int node_key, node *leaf)
 void tree::insert_node(int node_key)
 {
 	if (root != NULL)
-		insert(node_key, root)
+		insert_node(node_key, root);
 	else
 	{
 		root = new node;
 		root->value = node_key;
 		root->leftChild = NULL;
 		root->rightChild = NULL;
+		
 	}
 }
 
@@ -78,9 +87,9 @@ node *tree::search(int node_key, node *leaf)
 {
 	if(leaf!=NULL)
 	{
-		if(key == leaf->value)
+		if(node_key == leaf->value)
 			return leaf;
-		else if(key < leaf->value)
+		else if(node_key < leaf->value)
 			return search(node_key,leaf->leftChild);
 		else return search(node_key,leaf->rightChild);
 	}
@@ -89,17 +98,28 @@ node *tree::search(int node_key, node *leaf)
 
 node *tree::search(int node_key)
 {
-	return search(key, root);
+	return search(node_key, root);
 }
 
 void tree::preOrder(node *n)
 {
 	if (n != NULL)
 	{
-		cout << n->value << ", ";
+		cout << n->value << ", "<<endl;
 		preOrder(n->leftChild);
+		//cout << "printed a value" << endl;
 		preOrder(n->rightChild);
 	}
+	else 
+	{
+		//cout << "some problem";
+		return;
+	}
+}
+
+void tree::preOrder()
+{
+	return preOrder(root);
 }
 
 int tree::height(node *n)
@@ -116,3 +136,44 @@ int tree::height(node *n)
 	}
 }
 
+int tree::height()
+{
+	return height(root);
+}
+
+int main()
+{
+	tree binaryTree;
+
+	//inserting some values into the binary tree
+	binaryTree.insert_node(10);
+	binaryTree.insert_node(6);
+	binaryTree.insert_node(14);
+	binaryTree.insert_node(5);
+	binaryTree.insert_node(8);
+	binaryTree.insert_node(11);
+	binaryTree.insert_node(18);
+
+	//pre-order traversal
+	binaryTree.preOrder();
+
+	//search
+	//node *n = binaryTree.search(14);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
